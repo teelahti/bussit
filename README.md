5d5f4bc772ce9fd6e678b12add23c3a584557797
Tero Teelahti <tero@armadainteractive.com>
2018-10-09 15:51:50 +0300

    Change to look like buttons and take the whole vertical space

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Small personal website to show bus schedules for single route only (to overcome some limitations in the official site).
 
+## Develop
+
+Run a static web server, e.g. `cd public && python -m SimpleHTTPServer 8089`.
+As this site uses service workers you might need to clear them with your browser tools.
+
 ## Deploy
 
 To get proper HTTPS support for free, using Google Firebase hosting instead of Github pages.
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,44 +1,40 @@
-var cacheName = 'cache-v1';
-var urlsToCache = ['./'];
+var cacheName = "cache-v2";
+var urlsToCache = ["./"];
 
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cacheName)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-    return self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(urlsToCache))
+  );
+  return self.skipWaiting();
 });
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== cacheName) {
-                    return caches.delete(key);
-                }
-            }));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.open(cacheName).then(function (cache) {
-            return cache.match(event.request)
-                .then(function (response) {
-                    if (response) {
-                        console.log('Served from cache');
-                        return response;
-                    }
-                    return fetch(event.request).then(function (response) {
-                        console.log('Response from network is:', response);
-                        return response;
-                    });
-                }
-                )
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(response => {
+        if (response) {
+          console.log("Served from cache");
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          console.log("Response from network is:", response);
+          return response;
+        });
+      });
+    })
+  );
 });
